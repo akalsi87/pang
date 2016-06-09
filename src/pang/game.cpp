0a774bbda6ccd97bdcc240b0782a1f2ac0c1648d
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-06-09 09:01:14 -0400

    "Add paddles and ball"

--- src/pang/game.cpp
+++ src/pang/game.cpp
@@ -66,15 +66,133 @@ namespace pang {
         m_impl->window.close();
     }
 
-    static void gameIteration(GameData& data)
+    enum class Scaling
     {
+        ON,
+        OFF
+    };
+
+    template <Scaling sc = Scaling::OFF>
+    class GameObject;
+
+    template <>
+    class GameObject<Scaling::OFF> : public sf::Sprite
+    {
+      public:
+        GameObject()
+          : Sprite()
+          , m_texture()
+        {
+        }
+
+        GameObject(sf::Window&)
+          : Sprite()
+          , m_texture()
+        {
+        }
+
+        bool loadFromFile(const std::string& s)
+        {
+            if (m_texture.loadFromFile(getPath(s))) {
+                this->setTexture(m_texture);
+                return true;
+            }
+            return false;
+        }
+      private:
+        sf::Texture m_texture;
+    };
+
+    template <>
+    class GameObject<Scaling::ON> : public sf::Sprite
+    {
+      public:
+        GameObject(sf::Window& window)
+          : Sprite()
+          , m_windowSize(window.getSize())
+          , m_texture()
+        {
+        }
+
+        bool loadFromFile(const std::string& s)
+        {
+            if (m_texture.loadFromFile(getPath(s))) {
+                auto wsize = m_windowSize;
+                auto tsize = m_texture.getSize();
+                this->setTexture(m_texture);
+                this->scale((float)wsize.x/tsize.x, (float)wsize.y/tsize.y);
+                return true;
+            }
+            return false;
+        }
+
+        void setPosition(float, float)
+        {
+        }
+
+        void setPosition(const sf::Vector2f&)
+        {
+        }
+      private:
+        sf::Vector2u m_windowSize;
+        sf::Texture m_texture;
+    };
+
+    using Image = GameObject<Scaling::ON>;
+    using Item = GameObject<Scaling::OFF>;
+
+    struct GameplayData
+    {
+        Item paddleA;
+        Item paddleB;
+        Item ball;
+    };
+
+    template <class drawable>
+    void setCenterTo(drawable& d, sf::Vector2f pos)
+    {
+        auto tsize = d.getTexture()->getSize();
+        sf::Vector2f tfsize(tsize);
+        tfsize.x = tfsize.x / 2;
+        tfsize.y = tfsize.y / 2;
+        d.setPosition(pos - tfsize);
+    }
+
+    static bool setupGameplayData(sf::RenderWindow& window, GameplayData& game)
+    {
+        bool success = game.paddleA.loadFromFile("resources/paddle.png") &&
+                       game.paddleB.loadFromFile("resources/paddle.png") &&
+                       game.ball.loadFromFile("resources/ball.png");
+        if (!success) { return false; }
+
+        auto wsize = window.getSize();
+        sf::Vector2f wfsize(wsize);
+
+        setCenterTo(game.paddleA, { wfsize.x * 0.50f, wfsize.y * 0.75f });
+        setCenterTo(game.ball,    { wfsize.x * 0.50f, wfsize.y * 0.50f });
+        setCenterTo(game.paddleB, { wfsize.x * 0.50f, wfsize.y * 0.25f });
+
+        return true;
+    }
+
+    static void displayGameplayState(sf::RenderWindow& window, GameplayData& game)
+    {
+        window.clear();
+        window.draw(game.paddleA);
+        window.draw(game.paddleB);
+        window.draw(game.ball);
+        window.display();
+    }
+
+    static void gameIteration(GameData& data, GameplayData& game)
+    {
+        assert(data.state == GameState::PLAYING);
         auto& window = data.window;
         sf::Event ev;
         if (window.waitEvent(ev)) {
             switch (data.state) {
                 case GameState::PLAYING:
-                    window.clear(sf::Color(255, 0, 0));
-                    window.display();
+                    displayGameplayState(window, game);
                     if (ev.type == sf::Event::Closed) {
                         data.state = GameState::EXITING;
                     }
@@ -92,21 +210,15 @@ namespace pang {
     {
         assert(data.state == GameState::MENU);
 
-        sf::Texture img;
-        auto success = img.loadFromFile(getPath("resources/mainmenu.png"));
+        auto& window = data.window;
+
+        Image img(window);
+        auto success = img.loadFromFile("resources/mainmenu.png");
         assert(success);
         static_cast<void>(success);
 
-        sf::Sprite sprite(img);
-
-        auto& window = data.window;
-
-        auto wsize = window.getSize();
-        auto tsize = img.getSize();
-        sprite.scale((float)wsize.x/tsize.x, (float)wsize.y/tsize.y);
-
         window.clear();
-        window.draw(sprite);
+        window.draw(img);
         window.display();
 
         enum Buttons
@@ -164,16 +276,15 @@ namespace pang {
     {
         assert(data.state == GameState::SPLASH);
 
-        sf::Texture img;
-        auto success = img.loadFromFile(getPath("resources/splashscreen.png"));
+        auto& window = data.window;
+
+        Image img(window);
+        auto success = img.loadFromFile("resources/splashscreen.png");
         assert(success);
         static_cast<void>(success);
 
-        sf::Sprite sprite(img);
-
-        auto& window = data.window;
         window.clear();
-        window.draw(sprite);
+        window.draw(img);
         window.display();
 
         // wait for keypress | mouse button | exit
@@ -208,15 +319,22 @@ namespace pang {
             return;
         }
 
+        // show menu
         data.state = GameState::MENU;
         showMainMenu(data);
         if (data.state == GameState::EXITING) {
             return;
         }
 
+        // start game
+        GameplayData game;
+        bool success = setupGameplayData(data.window, game);
+        assert(success);
+        static_cast<void>(success);
+
         data.state = GameState::PLAYING;
         while (data.state != GameState::EXITING) {
-            gameIteration(data);
+            gameIteration(data, game);
         }
     }
 
